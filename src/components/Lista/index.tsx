d4aa83fc547d4aa557bf41067b13b9166e835496
Alan <3690-asherry@users.noreply.drupalcode.org>
2024-06-22 14:13:14 +0200

    terminar curso React: escrevendo com typescript

---
+++ src/components/Lista/index.tsx
@@ -0,0 +1,28 @@
+import style from './Lista.module.scss'
+import Item from './Item';
+import { ITarefa } from '../../types/tarefa';
+
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelectiona: ITarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+
+  return (
+    <aside className={style.listTarefas}>
+      <h2> Estudos do dia </h2>
+      <ul>
+        {tarefas.map((item) => (
+          <Item
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
+            {...item}
+          />
+        ))}
+      </ul>
+    </aside>
+  )
+}
+
+export default Lista;
